dcdd43bf2c187bb024a0c9c934ee5f0236300044
pyvan23 <ivanmiguelalvarez@gmail.com>
2022-06-17 13:28:16 -0300

    navBar setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-
+import { Home } from "./components/Home";
+import { NavBar } from "./components/NavBar";
 
 function App() {
   return (
-   <div><h1 className="text-4xl font-bold">Hello</h1></div>
+    <div>
+      <NavBar />
+      <Home />
+    </div>
   );
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import html5Img from "../assets/html5.png";
+import { FaArrowCircleRight } from "react-icons/fa";
+
+export const Home = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <h2>Hi there,I'm a fullstack web developer</h2>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Repudiandae, ipsa. Tempora, eos! Eos, exercitationem molestias.
+            Alias, inventore, exercitationem iste amet possimus dolorem veniam
+            culpa, corporis minima nisi sed ea doloribus.
+          </p>
+          <div>
+            <button>
+              Proyects <FaArrowCircleRight />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
