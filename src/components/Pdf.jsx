479f0c89aa7df1a853119fc664a9cc88e174d6a4
pyvan23 <ivanmiguelalvarez@gmail.com>
2022-06-18 13:07:10 -0300

    pdf render dependency

---
+++ src/components/Pdf.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+import {PDFDownloadLink, PDFViewer} from '@react-pdf/renderer'
+export const Pdf = () => {
+    const [view, setView] = useState(false)
+
+
+
+  return (
+    <div>Pdf</div>
+  )
+}
