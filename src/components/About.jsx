8fd1f655a2274df05609637cb1b4fb19e3ba8ef6
pyvan23 <ivanmiguelalvarez@gmail.com>
2022-06-19 19:52:16 -0300

    skills hover

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,11 +5,11 @@ export const About = () => {
     <div name="about" className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white ">
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div>
-          <p className="text-3xl font-bold ">About</p>
+          <p className="text-4xl font-bold border-b-4 border-gray-500 p-4 ">About</p>
         </div>
         <p className="text-xl mt-20">
         I am currently finishing my programming degree at the Teclab Higher Institute.
-I acquired experience in backend development at the Alkemy academy when we made a website for an NGO with a team of 8 people, where scrum was used as a work methodology, the technologies used for the project were node.js, express, mysql with sequelize like ORM and cypress for testing.
+I acquired experience in backend development at the Alkemy academy when we made a website for an NGO with a team of 8 people, where scrum was used as a work methodology, the technologies used for the project were node.js, express, mysql with sequelize like ORM,git,gitgub and cypress for testing.
 I have also developed other projects with the complete MERN stack, MongoDb, Express.js, React.js, Node.js.
         </p>
         <br />
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,7 +5,7 @@ import javascript from "../assets/javascript.png";
 import reactImage from "../assets/react.png";
 import node from "../assets/node.png";
 
- import mongo3 from "../assets/mongo3.png";
+import mongo3 from "../assets/mongo3.png";
 
 export const Skills = () => {
   const skills = [
@@ -13,37 +13,31 @@ export const Skills = () => {
       id: 1,
       src: html,
       title: "HTML",
-      style: "shadow-orange-500",
     },
     {
       id: 2,
       src: css,
       title: "CSS",
-      style: "shadow-blue-500",
     },
     {
       id: 3,
       src: javascript,
       title: "JavaScript",
-      style: "shadow-yellow-500",
     },
     {
       id: 4,
       src: reactImage,
       title: "React",
-      style: "shadow-blue-600",
     },
     {
       id: 5,
       src: mongo3,
       title: "Mongo",
-      style: "shadow-sky-400",
     },
     {
       id: 6,
       src: node,
       title: "Node JS",
-      style: "shadow-white",
     },
   ];
   return (
@@ -59,12 +53,9 @@ export const Skills = () => {
         </div>
 
         <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
-          {skills.map(({ id, src, title, style }) => (
-            <div
-              key={id}
-              className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
-            >
-              <img src={src} alt="" className="w-20 mx-auto" />
+          {skills.map(({ id, src, title }) => (
+            <div key={id}>
+              <img src={src}  alt="" className="w-20 mx-auto hover:scale-105 duration-200" />
               <p className="mt-4">{title}</p>
             </div>
           ))}
