38d414f2b02c37d4668daa956a9253c72f654dac
pyvan23 <ivanmiguelalvarez@gmail.com>
2022-08-07 18:37:44 -0300

    resume pdf test

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,7 +11,7 @@ export const Contact = () => {
       className="flex justify-between items-center w-full h-20 text-white bg-black "
     >
       <button className="bg-gray-800 hover:bg-gray-900 text-white font-bold  py-1 px-2 rounded inline-flex items-center m-4">
-        <a href={resume}  target="_blank"  rel="noopener noreferrer" download="nombre_pretendido_del archivo.pdf">
+        <a href={resume}  target="_blank"  rel="noopener noreferrer" download="resume fullstack software developer">
           Resume
         </a>
       </button>
